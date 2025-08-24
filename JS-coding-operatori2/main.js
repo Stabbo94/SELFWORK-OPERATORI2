c29c5609fbee90d006b87e4b39b995e6e6c60230
Stabbo94 <albertostaboli@gmail.com>
2025-08-24 21:42:31 +0200

    allego esercizio

---
+++ JS-coding-operatori2/main.js
@@ -0,0 +1,28 @@
+let annocorrente = 2025;
+let annonascita = 1992;
+
+// Calcolo età della persona:
+
+let age = annocorrente-annonascita;
+console.log(age);
+
+// Anni necessari a raggiungere i 100 anni di età:
+
+console.log(100-age);
+
+console.log(`Hai ${age} anni e ti mancano ${100-age} anni per compierne 100.`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
